3d5eea979dd26c35994ed27dd0e135156c39e175
Kordian <kordian.bober@wp.pl>
2019-07-15 15:25:16 +0200

    login

--- app/Http/Controllers/KartaController.php
+++ app/Http/Controllers/KartaController.php
@@ -12,6 +12,10 @@ class KartaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct(){
+      $this->middleware('auth');
+    }
+
     public function index()
     {
       // dd(Karta::find(1)->updated_at);
